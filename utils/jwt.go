dda0046cdde3f470d6861073a58cdf1b905df553
Victor Jimenez <victor.jimenez@LATAM-9QYQM0.local>
2025-02-06 09:16:19 -0500

    token validation ok

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +17,27 @@ func GenerateToken(email, user_id string) (string, error) {
 	})
 
 	return token.SignedString([]byte(apiKey))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("unespected signed method")
+		}
+		return []byte(apiKey), nil
+	})
+
+	if err != nil {
+		return errors.New("could not parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("token not valid")
+	}
+
+	return nil
+
 }
